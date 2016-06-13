e5eb7e3c07643431c41a3780db68d259818ec83c
Simina <coneac@gmail.com>
2016-06-13 14:30:09 +0300

    init
    Signed-off-by: Simina <coneac@gmail.com>

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+		<?php include "includes/head.php"; ?>
+	</head>
+
+	<body>
+		<?php include "includes/header.php"; ?>
+		<?php include "includes/footer.php"; ?>
+		<?php include "includes/js-global.php"; ?>
+	</body>
+</html>
